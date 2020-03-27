809701365a869e5096a84241d12d550a2ac116c0
FrancoASola <61510682+FrancoASola@users.noreply.github.com>
2020-03-26 20:06:35 -0600

    Delete checkingmongo.py

--- checkingmongo.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('mongodb+srv://Franco:SK81sc00@cluster0-3mgy4.gcp.mongodb.net/ScoresAroundWorld?retryWrites=true&w=majority')
-
-db = client.ScoresAroundWorld
-collection = db.coordinates
-
-print(collection.find_one({'name': 'test'}))
